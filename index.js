3155192ea2c5a927aeaad3131b703905e3b0fa9b
Yury Sinitskiy <unstable195@gmail.com>
2025-08-09 12:55:03 +0300

    update server fro vercel

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(
+	cors({
+		origin: "https://inventory-client-lac.vercel.app/",
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	})
+);
 app.use(express.json());
 
-
-
 app.post("/api/users", (req, res) => {
-	const user = 'hello world'
-    
+	const user = "hello world";
+
 	res.json({
 		user,
 	});
 });
-
 
 app.get("/", (req, res) => {
 	res.json({ status: "Server is working!" });
@@ -24,9 +27,9 @@ app.get("/api", (req, res) => {
 	res.json({ message: "API response" });
 });
 
-const PORT = 3001;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+// const PORT = 3001;
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+// })
 
 export default app;
