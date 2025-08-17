77897d3fbdb85b81f6f3cb94d5b7ffd22009f7a9
Yury Sinitskiy <unstable195@gmail.com>
2025-08-17 10:20:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,15 @@ const app = express();
 
 const allowedOrigins = ["https://inventory-client-lac.vercel.app"];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://inventory-client-lac.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204); // 👈 важно для Vercel
+  }
+  next();
+});
 
 // 👇 обязательно!
 app.options("*", cors());
@@ -27,13 +23,13 @@ app.options("*", cors());
 app.use(express.json());
 //app.use(cors());
 
-// app.post("/api/users", (req, res) => {
-// 	const user = "hello world";
+app.post("/api/users", (req, res) => {
+	const user = "hello world";
 
-// 	res.json({
-// 		user,
-// 	});
-// });
+	res.json({
+		user,
+	});
+});
 
 app.get("/", (req, res) => {
 	res.json({ status: "Server is working!" });
