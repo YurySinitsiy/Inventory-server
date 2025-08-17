3897505b5a1b7d36447b5fed364a98269a38e1ae
Yury Sinitskiy <unstable195@gmail.com>
2025-08-17 10:45:52 +0300

    update

--- src/prismaClient.js
+++ src/prismaClient.js
@@ -1,9 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+const { PrismaClient } = require('./generated/prisma')
 
-const globalForPrisma = globalThis;
-
-const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient()
 
 export default prisma;
