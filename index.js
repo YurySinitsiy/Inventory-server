8aa06b302df0db1981b6b4651fd4e4164857c7f2
Yury Sinitskiy <unstable195@gmail.com>
2025-09-12 18:48:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
       'http://localhost:5173',
       'https://inventory-client-lac.vercel.app',
       'https://invy2.odoo.com',
-      'https://invy.odoo.com',
+      //'https://invy.odoo.com',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-api-token'],
